fdbd4222029826d2836482d18fa13f9f42bf9c6c
Henrique Campos <henriquecampos@pigdev.studio>
2020-04-18 21:16:26 -0300

    add main menu

---
+++ source/Screens/MainMenu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+export (String, FILE, "*.tscn") var next_scene
+
+func _on_ButtonStart_pressed() -> void:
+	get_tree().change_scene(next_scene)
+
+
+
+func _on_ButtonQuit_pressed() -> void:
+	get_tree().quit()
