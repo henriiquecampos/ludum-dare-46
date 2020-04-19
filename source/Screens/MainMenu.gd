b5fdbc4cb997048dfcd349c14631d56a7beae376
Henrique Campos <henriquecampos@pigdev.studio>
2020-04-19 11:34:42 -0300

    add pause screen

--- source/Screens/MainMenu.gd
+++ source/Screens/MainMenu.gd
@@ -5,7 +5,3 @@ export (String, FILE, "*.tscn") var next_scene
 func _on_ButtonStart_pressed() -> void:
 	get_tree().change_scene(next_scene)
 
-
-
-func _on_ButtonQuit_pressed() -> void:
-	get_tree().quit()
